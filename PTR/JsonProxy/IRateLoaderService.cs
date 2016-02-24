65078e3636f874627222dbc64efaa5b823896a5d
kkrisztian2 <krisztian.kis@fx.hu>
2016-02-24 13:54:26 +0100

    Creation of Rate Loader Service
    Service gets data from MNB
    Returns rates in Json format

---
+++ PTR/JsonProxy/IRateLoaderService.cs
@@ -0,0 +1,13 @@
+﻿namespace JsonProxy
+{
+    using System.ServiceModel;
+    using System.ServiceModel.Web;
+    [ServiceContract]
+    public interface IRateLoaderService
+    {
+
+        [OperationContract]
+        [WebGet(UriTemplate = "Rates/Current", ResponseFormat =WebMessageFormat.Xml)]
+        string GetCurrentRates();
+    }
+}
---
+++ PTR/JsonProxy/RateLoaderService.svc.cs
@@ -0,0 +1,41 @@
+﻿using JsonProxy.MNBRateServiceReference;
+using Newtonsoft.Json;
+using System.IO;
+using System.Web.Script.Serialization;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace JsonProxy
+{
+    public class RateLoaderService : IRateLoaderService
+    {
+        public string GetCurrentRates()
+        {
+            var client = new MNBArfolyamServiceSoapClient();
+            var result = client.GetCurrentExchangeRates(new MNBRateServiceReference.GetCurrentExchangeRatesRequestBody());
+
+            var serializer = new XmlSerializer(typeof(JsonProxy.Contracts.MNBCurrentExchangeRates));
+
+
+
+            using (TextReader reader = new StringReader(result.GetCurrentExchangeRatesResult))
+            {
+                var xml = serializer.Deserialize(reader);
+
+                var json = new JavaScriptSerializer().Serialize(xml);
+
+                return json;
+
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(reader.ToString());
+                string jsonText = JsonConvert.SerializeXmlNode(doc);
+                return jsonText;
+
+
+
+            }
+
+            return result.GetCurrentExchangeRatesResult;
+        }
+    }
+}
