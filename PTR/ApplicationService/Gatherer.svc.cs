83480ba64ac38b73160e7bf5ffa5bc12241b488d
martinczifra <czifra.martin@gmail.com>
2016-03-08 19:36:46 +0100

    Created skeleton of application service

---
+++ PTR/ApplicationService/Gatherer.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ApplicationService
+{
+    public class Gatherer : IGatherer
+    {
+        public void GatherCurrentRates()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
